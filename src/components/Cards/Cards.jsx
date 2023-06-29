02147aa2a6491bf2a393950d7466c905b0afe040
Joaquingro <joaquinsgro@gmail.com>
2023-06-28 20:05:58 -0600

    Animation flipIn

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -4,14 +4,25 @@ import star from "../../assets/star.svg";
 import sun from "../../assets/sun.svg";
 import comet from "../../assets/comet.svg";
 import { useEffect, useState } from "react";
+import 'animate.css';
+
 
 export function Cards(){
     const [image, setImage] = useState([]);
-    const [flip, setFlip] = useState(false);
+    const imageArray = [
+        { id: 1, img: moon, flipped: false, text: "The moon" },
+        { id: 2, img: star, flipped: false, text: "The star" },
+        { id: 3, img: sun, flipped: false, text: "The sun" },
+        { id: 4, img: comet, flipped: false, text: "The comet" },
+        { id: 5, img: moon, flipped: false, text: "The moon" },
+        { id: 6, img: star, flipped: false, text: "The star" },
+        { id: 7, img: sun, flipped: false, text: "The sun" },
+        { id: 8, img: comet, flipped: false, text: "The comet" }
+    ];
+
 
     useEffect(() => {
-        const imageArray = [moon, star, sun, comet];
-        const doubledImages = [...imageArray, ...imageArray];
+        const doubledImages = [...imageArray];
         setImage(doubledImages)
         shuffleImages(doubledImages);
     }, [])
@@ -25,8 +36,15 @@ export function Cards(){
         setImage(shuffledImages);
       };
 
-    const handleFlip = () => {
-        setFlip(true)
+    const handleFlip = (cardId) => {
+        setImage((prevImage) => {
+            return prevImage.map((card) => {
+              if (card.id === cardId) {
+                return { ...card, flipped: true };
+              }
+              return card;
+            });
+          });
     }
    
 
@@ -34,17 +52,18 @@ export function Cards(){
     <>
     <div  className="container text-center">
         <div  className="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-        {image && image.length > 0 && image.map((img, index) => (
+        {image && image.length > 0 && image.map((card, index) => (
         <div key={index} className="col mb-4">
             
-                {flip ? 
-                <div className={`card ${styles.cardContainerFront}`}>
-                    <div className="card-front">
-                        <img src={img} className="card-img-top" alt="card"/>
+                {card.flipped ? 
+                <div className={`card ${styles.cardContainerFront} ${card.flipped ? styles['flip-in-up'] : ''}`}>
+                    <div id = {index} className={`card-front ${styles.cardFront}`}>
+                        <img src={card.img} className="card-img-top" alt="card"/>
+                        <p>{card.text}</p>
                     </div>
                 </div> : 
-                <div className={`card ${styles.cardContainerBack}`}>
-                    <div className={`card-back ${styles.cardBack}`} onClick={handleFlip}>
+                <div className={`card ${styles.cardContainerBack} ${card.flipped ? styles['flip-out-up'] : ''}`} onClick={() => handleFlip(card.id)}>
+                    <div className={`card-back ${styles.cardBack} `} >
                         <h1>?</h1>
                     </div>
                 </div>}
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,22 +4,35 @@ import { Cards } from "../Cards/Cards.jsx";
 import soundOn from "../../assets/sound--on.svg";
 import soundOff from "../../assets/sound--off.svg";
 import soundBackground from "../../assets/background.mp3";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 export function Home () {
     const [isSoundOn, setIsSoundOn] = useState(true);
+    const audioRef = useRef(new Audio(soundBackground));
 
-    const toggleSound = () => {
-      setIsSoundOn((prevIsSoundOn) => !prevIsSoundOn);
-    };
-  
+    useEffect(() => {
+    const audio = audioRef.current;
+        if (isSoundOn) {
+             audio.play(); 
+        } else {
+            audio.pause(); 
+        }
+    }, [isSoundOn])
+    
+  const toggleSound = () => {
+    setIsSoundOn(!isSoundOn);
+  };
+
+
+
     return(
     <>
     <h1>Let's Start!</h1>
     <p>To start click on a card</p>
     <div className={styles.buttons}>
         <button className = {styles.buttonSound} onClick={toggleSound}>
+        <audio ref={audioRef} src={soundBackground} loop={true} />
         {isSoundOn ? (
             <img src={soundOn} alt="Sound On" />
             ) : (
